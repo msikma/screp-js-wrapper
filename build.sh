9a1caebe6a2a40f8739d619f7e938571a95c34fb
Michiel Sikma <michiel@sikma.org>
2022-02-12 15:27:45 +0100

    Print version while building

--- build.sh
+++ build.sh
@@ -8,4 +8,5 @@ if ! command -v gopherjs &> /dev/null; then
     exit 1
 fi
 
+echo "icza/screp" $(cat ./go.mod | grep "^\s\+github.com/icza/screp" | head -n 1 | grep -o "v\(.*\)")
 gopherjs build -vo "$BASEDIR"/dist/index.js "$BASEDIR"/src/main.go
