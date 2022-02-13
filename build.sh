d9d55cf644a9b57438909eba5ea09b9ae512f63a
Michiel Sikma <michiel@sikma.org>
2022-02-13 19:04:27 +0100

    Don't check for $modules - assume it's there

--- build.sh
+++ build.sh
@@ -3,14 +3,14 @@
 BASEDIR=$(dirname "$0")
 
 if ! command -v gopherjs &> /dev/null; then
-    echo "error: gopherjs command not available."
-    echo "Install it from here: <https://github.com/gopherjs/gopherjs>"
-    exit 1
+  echo "error: gopherjs command not available."
+  echo "Install it from here: <https://github.com/gopherjs/gopherjs>"
+  exit 1
 fi
 
 echo "icza/screp" $(cat "$BASEDIR"/go.mod | grep "^\s\+github.com/icza/screp" | head -n 1 | grep -o "v\(.*\)")
 gopherjs build -vo "$BASEDIR"/dist/index.js "$BASEDIR"/src/main.go
 rm "$BASEDIR"/dist/index.js.map
-sed 's/$module.exports.ScrepJS/if ($module) $module.exports.ScrepJS/g' "$BASEDIR"/dist/index.js | sed 's/^\/\/# sourceMappingURL=\(.*\)//g' > "$BASEDIR"/dist/_index.js
+sed 's/^\/\/# sourceMappingURL=\(.*\)//g' "$BASEDIR"/dist/index.js > "$BASEDIR"/dist/_index.js
 rm "$BASEDIR"/dist/index.js
 mv "$BASEDIR"/dist/_index.js "$BASEDIR"/dist/index.js
