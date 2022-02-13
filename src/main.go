8aee90239577394ae564f69c9586fafa2c73b654
Michiel Sikma <michiel@sikma.org>
2022-02-13 19:04:51 +0100

    Remove JSON encoding; add getVersion

--- src/main.go
+++ src/main.go
@@ -1,14 +1,23 @@
 package main
 
 import (
-	"encoding/json"
+	"runtime"
 
 	"github.com/gopherjs/gopherjs/js"
+	"github.com/icza/screp/rep"
 	"github.com/icza/screp/repparser"
 )
 
+// Note: copied from icza/screp/cmd/screp.
+const (
+	appName    = "screp"
+	appVersion = "v1.5.1"
+	appAuthor  = "Andras Belicza"
+	appHome    = "https://github.com/icza/screp"
+)
+
 // Parses a binary buffer as through it's a .rep file and returns an object with the result.
-func parseBuffer(buffer []byte) (map[string]interface{}, *string) {
+func parseBuffer(buffer []byte) (*rep.Replay, *string) {
 	res, err := repparser.ParseConfig(buffer, repparser.Config{Commands: true, MapData: true})
 
 	if err != nil {
@@ -19,24 +28,27 @@ func parseBuffer(buffer []byte) (map[string]interface{}, *string) {
 	// Add computed properties.
 	res.Compute()
 
-	// Convert to JSON and back to mimic the exact structure provided by the command line tool.
-	jsonRes, err := json.Marshal(res)
-	if err != nil {
-		errStr := err.Error()
-		return nil, &errStr
-	}
-	
-	var resMap map[string]interface{}
-	json.Unmarshal([]byte(jsonRes), &resMap)
-	return resMap, nil
+	return res, nil
 }
 
-// Declares the ScrepJS object on the global object.
+// Returns the same version information that is also provided by the screp command line tool,
+// as an array of items instead of a single string.
+func getVersion() ([][]string) {
+	return [][]string{
+		{(appName + " version"), appVersion},
+		{"Parser version", repparser.Version},
+		{"EAPM algorithm version", rep.EAPMVersion},
+		{"Platform", runtime.GOOS + " " + runtime.GOARCH},
+		{"Built with", runtime.Version()},
+		{"Author", appAuthor},
+		{"Home page", appHome},
+	}
+}
+
+// Exports the ScrepJS object as a CommonJS module.
 func main() {
-	js.Global.Set("ScrepJS", map[string]interface{}{
-		"parseBuffer": parseBuffer,
-	})
 	js.Module.Get("exports").Set("ScrepJS", map[string]interface{}{
 		"parseBuffer": parseBuffer,
+		"getVersion": getVersion,
 	})
 }
